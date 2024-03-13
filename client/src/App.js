1b85fe49451506caf8a502579ddf2fc2d2d9b0c8
Carolina Jimenez Moreno <CJimenezm0794@gmail.com>
2024-03-13 15:44:44 -0500

    Server - Improve logic and response (sending result and computer action) | Client - Improve view and logic

--- client/src/App.js
+++ client/src/App.js
@@ -2,48 +2,110 @@
 Rock-Paper-Scissors Game
 
 References:
-- https://www.youtube.com/watch?v=7LNl2JlZKHA
-- https://www.youtube.com/watch?v=PppslXOR7TA
-- https://react-icons.github.io/react-icons/search/#q=hand
+- How to Create a Flask + React Project | Python Backend + React Frontend - https://www.youtube.com/watch?v=7LNl2JlZKHA
+- Python + JavaScript - Full Stack App Tutorial - https://www.youtube.com/watch?v=PppslXOR7TA
+- React Icons - https://react-icons.github.io/react-icons/
+- Rock Paper Scissors React Tutorial in 12 Minutes - https://www.youtube.com/watch?v=7G7n1SnHnDk
 */ 
 
 import React, { useState, useEffect } from 'react';
 import { FaHand, FaHandPeace, FaHandFist } from "react-icons/fa6";
 import start from './images/start.png';
-import paper from './images/paper.gif';
 import rock from './images/rock.gif';
+import paper from './images/paper.gif';
 import scissors from './images/scissors.gif';
 import './App.css';
 
+function ActionButton({action, onActionSelected}) {
+  const ICONS = {
+    "r": <FaHandFist className="icon" />,
+    "p": <FaHand className="icon" />,
+    "s": <FaHandPeace className="icon" />,
+  }
+  const Icon = ICONS[action]
+
+  const handleClick = () => {
+    // Enviar acción al servidor Flask
+    fetch('/play_game', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ action }),
+    })
+    .then(response => response.json())
+    .then(response => {
+      // Manejar la respuesta del servidor
+      onActionSelected(response, action);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  };
+
+  return (
+    <button className="round-btn" onClick={handleClick}>
+      {Icon}
+    </button>
+  )
+}
+
+function ActionIcon({name, action}) {
+  const ICONS = {
+    "start": {"icon": start, "word": "start"},
+    "r": {"icon": rock, "word": "rock"},
+    "p": {"icon": paper, "word": "paper"},
+    "s": {"icon": scissors, "word": "scissors"},
+  }
+  const icon = ICONS[action]["icon"]
+  const action_word = ICONS[action]["word"]
+  const clase = `${action_word}-${name == "Player" ? "user" : "pc" }-icon`
+  return (
+    <img src={icon} className={clase} alt="icon" />
+  )
+}
+
+function Player({name = "Player", action="start"}) {
+  return (
+  <div className="player">
+    <div className="score">{`${name}`}</div>
+    <div className="action">
+        {action && <ActionIcon name={name} action={action} />}
+    </div>
+  </div>
+  )
+}
+
 export default function App() {
-    return (
-      <div className="App">
-        <header className="App-container">
-          <h1>Rock Paper Scissors</h1>
-          <div>
-            <div className="container">
-                <div className="player">
-                    <div className="score">Player: 0</div>
-                    <div>
-                        <img src={start} className="start-user-icon" alt="start" />
-                    </div>
-                </div>
-                <h2>vs</h2>
-                <div className="player">
-                    <div className="score">Computer: 0</div>
-                    <div>
-                        <img src={start} className="start-pc-icon" alt="start" />
-                    </div>
-                </div>
-            </div>
-            <div className="action-btn">
-                <button className="round-btn"><FaHandFist  className="icon" /></button>
-                <button className="round-btn"><FaHand  className="icon" /></button>
-                <button className="round-btn"><FaHandPeace  className="icon" /></button>
-            </div>
-            <h2>Player wins!</h2>
+  const [playerAction, setPlayerAction] = useState("start");
+  const [computerAction, setComputerAction] = useState("start");
+
+  const [winner, setWinner] = useState("");
+
+  const onActionSelected = (response, selectedAction) => {
+    setPlayerAction(selectedAction);
+    setComputerAction(response['computer_action'])
+    setWinner(response['result'])
+  }
+
+  return (
+    <div className="App">
+      <header className="App-container">
+        <h1>Rock Paper Scissors</h1>
+        <div>
+          <div className="container">
+              <Player name="Player" action={playerAction} />
+              <h2>vs</h2>
+              <Player name="Computer" action={computerAction} />
           </div>
-        </header>
-      </div>
-    );
+          <div className="action-btn">
+              <ActionButton action="r" onActionSelected={onActionSelected} />
+              <ActionButton action="p" onActionSelected={onActionSelected} />
+              <ActionButton action="s" onActionSelected={onActionSelected} />
+          </div>
+          <h2>{winner}</h2>
+        </div>
+      </header>
+    </div>
+  );
 }
--- flask-server/server.py
+++ flask-server/server.py
@@ -3,19 +3,17 @@
 Rock-Paper-Scissors Game
 
 References:
-- https://www.youtube.com/watch?v=7LNl2JlZKHA
-- https://www.youtube.com/watch?v=PppslXOR7TA
+- How to Create a Flask + React Project | Python Backend + React Frontend - https://www.youtube.com/watch?v=7LNl2JlZKHA
+- Python + JavaScript - Full Stack App Tutorial - https://www.youtube.com/watch?v=PppslXOR7TA
 """
 
-__author__ = "Carolina Jiménez Moreno <cjimenezm0794@gmail.com>"
 __version__ = "1.0.0"
 
 # Standard library imports.
-import json
 import random
 
 # Third party imports.
-from flask import Flask, jsonify
+from flask import Flask, request
 
 
 # * * *
@@ -25,26 +23,29 @@ app = Flask(__name__)
 
 # * * *
 # ENDPOINTS
-@app.route("/", methods=["GET"])
-def home():
-    game_details = {"games": ["rock", "paper", "scissors"]}
-    return jsonify(game_details)
-
-@app.route("/set_user_choise", methods=["GET"])
-def set_user_choise():
-    user = input("What's your choise?: 'r' for rock, 'p' for paper, 's' for scissors\n")
-    return user
-
-@app.route("/get_pc_choise", methods=["GET"])
-def get_pc_choise():
-    computer = random.choice(['r', 'p', 's'])
-    return computer
-
 @app.route("/play_game", methods=["POST"])
 def play_game():
-    if user == computer:
-        return "It's a tie"
+    user_action = request.json['action']
+    computer_action = random.choice(['r', 'p', 's'])
+    if user_action == computer_action:
+        return {"result": "It's a tie", "computer_action": computer_action}
+
+    if get_winner(user_action, computer_action):
+        return {"result": "Player wins!", "computer_action": computer_action}
+
+    return {"result": "Player lost!", "computer_action": computer_action}
 # * * *
+
+# * * *
+# FUNCTIONS
+def get_winner(player, opponent):
+    if (player == 'r' and opponent == 's') or \
+    (player == 's' and opponent == 'p') or \
+        (player == 'p' and opponent == 'r'):
+        return True
+    return False
+# * * *
+
 
 if __name__ == "__main__":
     app.run(debug=True)
