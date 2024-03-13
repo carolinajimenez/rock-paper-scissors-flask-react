c8ec4a9b33741536d130d0b6227fbb886647e036
Carolina Jimenez Moreno <CJimenezm0794@gmail.com>
2024-03-12 20:52:42 -0500

    Client - Update interface

--- client/src/App.js
+++ client/src/App.js
@@ -4,31 +4,46 @@ Rock-Paper-Scissors Game
 References:
 - https://www.youtube.com/watch?v=7LNl2JlZKHA
 - https://www.youtube.com/watch?v=PppslXOR7TA
+- https://react-icons.github.io/react-icons/search/#q=hand
 */ 
+
 import React, { useState, useEffect } from 'react';
+import { FaHand, FaHandPeace, FaHandFist } from "react-icons/fa6";
+import start from './images/start.png';
+import paper from './images/paper.gif';
+import rock from './images/rock.gif';
+import scissors from './images/scissors.gif';
+import './App.css';
 
 export default function App() {
-    const [data, setData] = useState({});
-
-    useEffect(() => {
-        fetch("/game", {method: 'GET'}).then(
-            (res) => res.json()
-        ).then(
-            (data) => setData(data)
-        ).catch((error) => {
-            console.error('Error fetching data:', error);
-        });
-    }, []);
-
     return (
-        <div>
-            {(typeof data.games === 'undefined') ? (
-                <p>Loading ...</p>
-            ): (
-                data.games.map((game, i) => (
-                    <p key={i}>{game}</p>
-                ))
-            )}
-        </div>
-    )
+      <div className="App">
+        <header className="App-container">
+          <h1>Rock Paper Scissors</h1>
+          <div>
+            <div className="container">
+                <div className="player">
+                    <div className="score">Player: 0</div>
+                    <div>
+                        <img src={start} className="start-user-icon" alt="start" />
+                    </div>
+                </div>
+                <h2>vs</h2>
+                <div className="player">
+                    <div className="score">Computer: 0</div>
+                    <div>
+                        <img src={start} className="start-pc-icon" alt="start" />
+                    </div>
+                </div>
+            </div>
+            <div className="action-btn">
+                <button className="round-btn"><FaHandFist  className="icon" /></button>
+                <button className="round-btn"><FaHand  className="icon" /></button>
+                <button className="round-btn"><FaHandPeace  className="icon" /></button>
+            </div>
+            <h2>Player wins!</h2>
+          </div>
+        </header>
+      </div>
+    );
 }
